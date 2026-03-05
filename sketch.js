23ebd90a205947627f7d08434ab0a9a552d6adf7
oswordhuang-sys <oswordhuang@gmail.com>
2026-03-05 19:42:41 +0000

    Add basic sketch setup and draw functions

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+createCanvas(1024,768);
+}
+
+function draw(){
+background(0);
+
+fill(255);
+textSize(40);
+textAlign(CENTER,CENTER);
+
+text("Final Project",width/2,height/2);
+}
