40ee12b3fa7e5ec99daae2ad2e15fa2ef526a56c
oswordhuang-sys <oswordhuang@gmail.com>
2026-03-11 21:49:19 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let xs = [];
 let ys = [];
 
+let connections = [];
+
 let n = 10;
 let selected = -1;
 
@@ -29,9 +31,9 @@ function setup() {
 }
 
 function draw() {
-  background(20);
+  drawBackground();
 
-  // nodes
+  // nodes all
   for (let i = 0; i < n; i++) {
     // shaking
     xs[i] = xs[i] + random(-0.5, 0.5);
@@ -42,10 +44,13 @@ function draw() {
     ellipse(xs[i], ys[i], 20, 20);
   }
 
-  // connection
-  if (connected) {
-    stroke(255);
-    line(x1, y1, x2, y2);
+  // draw all connections
+  stroke(255);
+  for (let c of connections) {
+    
+    //Regarding the display issue of the connection, I asked ChatGPT5.3 to make the necessary changes. This is because initially, my links would disappear immediately after the next connection was triggered.
+    
+    line(c.x1, c.y1, c.x2, c.y2);
   }
 
   fill(200);
@@ -63,19 +68,48 @@ function mousePressed() {
       if (selected == -1) {
         selected = i;
       } else {
+
         x1 = xs[selected];
         y1 = ys[selected];
 
         x2 = xs[i];
         y2 = ys[i];
+
         let rate = random();
+
         if (rate < 0.5) {
-          connected = true;
-        } else {
-          connected = false;
+          connections.push({
+            x1: x1,
+            y1: y1,
+            x2: x2,
+            y2: y2
+          });
         }
+
         selected = -1;
       }
     }
+  }
+}
+
+// Draw a gradient background with noise points.
+function drawBackground() {
+  
+  //Regarding the issue of displaying noise in this step, I consulted ChatGPT5.3 to make the necessary changes because initially, the noise was almost impossible to display.
+  
+  for (let i = 0; i <= height; i++) {
+    let inter = map(i, 0, height, 0, 1);
+    let c = lerpColor(color(20, 20, 40), color(40, 40, 80), inter);
+    stroke(c);
+    line(0, i, width, i);
+  }
+  strokeWeight(3);
+  
+  for (let i = 0; i < 500; i++) {
+    let px = random(width);
+    let py = random(height);
+    let alpha = random(20, 50);
+    stroke(255, 255, 255, alpha);
+    point(px, py);
   }
 }
