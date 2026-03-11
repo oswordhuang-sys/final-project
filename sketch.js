3458203b7388fdea2d706cb95d122455545a6ab0
oswordhuang-sys <oswordhuang@gmail.com>
2026-03-11 19:00:37 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 let xs = [];
 let ys = [];
 
-let nodeCount = 10;
-
+let n = 10;
 let selected = -1;
 
-let xA, yA, xB, yB;
+let x1 = 0;
+let y1 = 0;
+let x2 = 0;
+let y2 = 0;
+
 let connected = false;
+let sound;
 
 function preload() {
   sound = loadSound("pong.wav");
@@ -15,78 +19,63 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  //（this “Array value retrieval- xs[i]，ys[i]” was made by ChatGbt5.3）
-  for (let i = 0; i < nodeCount; i++) {
+  for (let i = 0; i < n; i++) {
     xs[i] = random(100, width - 100);
     ys[i] = random(100, height - 100);
-    xs[i] += random(-0.3,0.3);
-ys[i] += random(-0.3,0.3);
+
+    xs[i] = xs[i] + random(-0.3, 0.3);
+    ys[i] = ys[i] + random(-0.3, 0.3);
   }
 }
 
 function draw() {
   background(20);
 
-  fill(255);
-  noStroke();
+  // nodes
+  for (let i = 0; i < n; i++) {
+    // shaking
+    xs[i] = xs[i] + random(-0.5, 0.5);
+    ys[i] = ys[i] + random(-0.5, 0.5);
 
-  // draw nodes
-  for (let i = 0; i < nodeCount; i++) {
-
-    // emergent movement
-    xs[i] += random(-0.5, 0.5);
-    ys[i] += random(-0.5, 0.5);
-
-    circle(xs[i], ys[i], 20);
+    fill(255);
+    noStroke();
+    ellipse(xs[i], ys[i], 20, 20);
   }
 
-  // draw connection
+  // connection
   if (connected) {
     stroke(255);
-    line(xA, yA, xB, yB);
+    line(x1, y1, x2, y2);
   }
 
   fill(200);
   textAlign(CENTER);
-  text("Click two nodes to try connecting them", width/2, 40);
+  text("Click two nodes to try connecting them", width / 2, 40);
 }
 
 function mousePressed() {
-  
   sound.play();
 
-  for (let i = 0; i < nodeCount; i++) {
-
+  for (let i = 0; i < n; i++) {
     let d = dist(mouseX, mouseY, xs[i], ys[i]);
 
     if (d < 15) {
-
-      if (selected === -1) {
-
+      if (selected == -1) {
         selected = i;
-
       } else {
-        
-     //（this “Array value retrieval”was made by ChatGbt5.3）
+        x1 = xs[selected];
+        y1 = ys[selected];
 
-        xA = xs[selected];
-        yA = ys[selected];
-
-        xB = xs[i];
-        yB = ys[i];
-
-        // protocol rule
-        if (random() < 0.5) {
+        x2 = xs[i];
+        y2 = ys[i];
+        let rate = random();
+        if (rate < 0.5) {
           connected = true;
         } else {
           connected = false;
         }
-
         selected = -1;
       }
-
     }
-
   }
-
 }
